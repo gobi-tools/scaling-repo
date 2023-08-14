ae8ffdbc6e3637b83e6caca0f5e86e67bd7d232c
devgabrielcoman <dev.gabriel.coman@gmail.com>
2023-08-14 09:12:17 +0100

    the second load test

---
+++ test-2/src/server.ts
@@ -0,0 +1,47 @@
+import express from 'express';
+import 'reflect-metadata';
+import { Column, DataSource, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+const app = express();
+
+const PORT = 3000;
+const DOMAIN = "server";
+const DB_HOST = process.env.DB_HOST;
+
+const router = express.Router();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+@Entity()
+export class Result {
+  @PrimaryColumn('uuid')
+  id: string
+
+  @Column()
+  value: number
+}
+
+const dataSource = new DataSource({ type: 'postgres', url: DB_HOST, entities: [Result], synchronize: true });
+
+router.post('/create', async (req, res) => {
+  const id = uuid();
+  const value = Math.floor(Math.random() * 100) + 1; // random number between 1 and 100
+  const entity = dataSource.manager.create(Result, { id, value });
+  await dataSource.manager.save(entity);
+  res.status(200).json({ success: true, result: { id, value } });
+});
+
+router.get('', async (req, res) => {
+  const result = await dataSource.manager.find(Result);
+  res.status(200).json({ success: true, result });
+});
+
+app.use(`/${DOMAIN}`, router);
+
+app.listen(PORT, async () => {
+  console.log(`App Server listening on port ${PORT}`);
+  await dataSource.initialize();
+  console.log(`Connected to DB on ${DB_HOST}`);
+});
--- test-5/README.md
+++ test-5/README.md
@@ -4,38 +4,20 @@ README
 You'll need a couple of things installed:
 
 - [Docker](https://www.docker.com/)
-- [Docker Compose](https://docs.docker.com/compose/)
 - [K6](https://k6.io/docs/)
 - [awk](https://www.gnu.org/software/gawk/manual/gawk.html)
 - [jq](https://jqlang.github.io/jq/)
-- [Node](https://nodejs.org/en)
 
-You will need to install dependencies in the `src` folder
+You can build the project from the `src` folder
 
 ```bash
-# via npm
-npm i 
-
-# via yarn
-yarn install
+docker-compose up --build --remove-orphans
 ```
 
-Then you can build & run the project via `docker compose`
+and you can run it via
 
 ```bash
-docker-compose down -v && docker-compose up --build --remove-orphans
-```
-
-To create new entries in the DB:
-
-```bash
-curl --request POST http://localhost/server/create
-```
-
-and to get all entries in the DB
-
-```bash
-curl http://localhost/server
+docker run --name my-server --cpus="1.0" --memory="0.2g" -p 80:3000 my-server
 ```
 
 To run the load test, edit `loadtest.js` to whichever `target` and `duration` you want, and then:
